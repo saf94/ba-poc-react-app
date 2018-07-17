92ee941006accac0fa501c5b49786fb788926d47
Safeer Hayat <saf_hayat94@hotmail.com>
2018-07-17 21:10:01 +0100

    minor tweaks

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -5,10 +5,10 @@ function Home() {
   return (
     <div>
       <h1>Home</h1>
-      This project is running Angular 6
+      This project is running ReactJS
       <div className="mt-2">Examples:</div>
       <ul>
-        <li>Angular 6 architecture</li>
+        <li>ReactJS architecture</li>
         <li>Routing including 404 pages and automatic redirection</li>
         <li>Components creation</li>
         <li>
