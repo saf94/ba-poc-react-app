beb4d07b0469498f0a5d43680111c9ff8916166d
Safeer Hayat <saf_hayat94@hotmail.com>
2018-07-17 16:52:44 +0100

    added skeleton of app and routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Home from "./Components/Home";
+import Upgrade from "./Components/Upgrade";
+import Redirect from "./Components/Redirect";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/upgrade" component={Upgrade} />
+          <Route component={Redirect} />
+        </Switch>
+      </Router>
+    );
   }
 }
 
---
+++ src/Components/Redirect/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Redirect() {
+  return <div>Redirect</div>;
+}
+
+export default Redirect;
